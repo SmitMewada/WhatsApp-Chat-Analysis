6e96005f643d85272bb1957b82e84e885dac716d
Smit Mewada <smitmewada009@gmail.com>
2023-12-17 20:46:35 -0500

    Added remove_unicode \u200e

--- preprocessor.py
+++ preprocessor.py
@@ -16,4 +16,14 @@ def preprocess(input_string):
     df["day_of_the_month"] = df.date.dt.day
     df["hour"] = df.date.dt.hour
     df["minute"] = df.date.dt.minute
+    
+    # Remove specific unicode
+    df["message"] = df["message"].apply(remove_specific_unicode)
     return df
+
+def remove_specific_unicode(input_text):
+ 
+    regex_pattern = '[' + re.escape("\u200e") + ']'
+    cleaned_text = re.sub(regex_pattern, '', input_text)
+    
+    return cleaned_text
