0b67f57b44024a137caee3d44ab9ff01ceeee51e
Smit Mewada <smitmewada009@gmail.com>
2023-12-17 19:01:08 -0500

    Added get_links_count()

--- helper.py
+++ helper.py
@@ -1,3 +1,5 @@
+from urlextract import URLExtract
+
 def fetch_stats(user, df):
     '''
         The fetch_stats fetch stats for the specific user.
@@ -16,3 +18,28 @@ def get_word_count(user, df):
         return df["message"].apply(lambda x: len(x.split())).sum()
     else:
         return df[df["user"] == user]["message"].apply(lambda x: len(x.split())).sum()
+    
+    
+def get_links_count(user, df):
+    links = []
+    extractor = URLExtract()
+    
+    if user == "Overall":
+        for message in df["message"]:
+            links.extend(extractor.find_urls(message))
+            
+    else:
+        for message in df[df["user"] == user]["message"]:
+            links.extend(extractor.find_urls(message))
+            
+    return len(links)
+    
+def get_image_count(user, df):
+    '''
+        This get_image_count counts the number of images shared by the person.
+    '''
+
+    if user == "Overall":
+        return df[df["message"] == "Yes"]["message"].count()
+    else:
+        return df[df["message"] == 'Yes']["message"].count()
